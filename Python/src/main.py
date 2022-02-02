994ef379dcd38b01476d4972baedd5531a5d2d24
El <63125641+shireishi@users.noreply.github.com>
2022-02-02 08:51:04 -0700

    Creating file structure and basic notes

---
+++ Python/src/main.py
@@ -0,0 +1,22 @@
+import socket
+import threading
+import asyncio
+
+"""
+Server Architecture Notes:
+    The server architecture will be primarily composed of a defined header that will contain the information
+    about the length of the incoming information, which can range a very large data set.
+
+    Custom communication protocols will be made, as well as repeating data with the header toggle of a resend
+    to denote whether or not all of the original information was recieved. 
+
+    There should also be a hash check system where a hash is sent along with the information, the information
+    is hashed by the same algorithm and then compared to make sure the correct data was recieved.
+"""
+
+def server_start():
+    pass
+
+
+
+server_start()
