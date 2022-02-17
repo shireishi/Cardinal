4c3052587f05d4a64cc0c77af1e9e3c352f6289f
El <63125641+shireishi@users.noreply.github.com>
2022-02-17 10:19:10 -0700

    Simplifying some stupid code

--- Python/src/security.py
+++ Python/src/security.py
@@ -1,9 +1,13 @@
 def hash(data):
-    if isinstance(data, str) != True:data = str(data)
-    data = [x for x in data]
+    """
+    Custom hashing function that uses the length of the data stream
+    and the data ordinal and integer values as the parameteres with the
+    modulus operator.
+    """
+    data = list(str(data))
     for loc, value in enumerate(data):
         try:data[loc] = str(int(value))
         except:data[loc] = str(ord(value))
         else:return 1
     for loc, num in enumerate(data):data[loc] = int(num)%len(data)*(loc+1)
-    return hex(int("".join(str(n) for n in data))).replace('0x', '')
+    return hex(int("".join(str(n) for n in data))).replace('0x', '') 
